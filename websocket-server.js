7087e2daa75e9681229e5dad1b6cc7011c458bca
Vinh Nguyen <VinhNuwin@protonmail.com>
2024-04-28 15:48:53 -0700

    added ping-pong to keep connection alive

--- websocket-server.js
+++ websocket-server.js
@@ -3,6 +3,22 @@ const app = express();
 const WebSocket = require("ws");
 const cors = require("cors");
 
+const interval = setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(null, false, true);
+  });
+}, 30000); // Ping every 30 seconds
+
+wss.on("connection", (ws) => {
+  ws.isAlive = true;
+  ws.on("pong", () => {
+    ws.isAlive = true;
+  });
+});
+
 // Configuring CORS
 app.use(
   cors({
@@ -68,49 +84,3 @@ process.on("SIGTERM", () => {
     console.log("Process exited");
   });
 });
-
-// const express = require("express");
-// const app = express();
-// const WebSocket = require("ws");
-// const cors = require("cors");
-
-// // Configuring CORS
-// app.use(
-//   cors({
-//     // Optionally, specify allowed origins, methods, etc.
-//     origin: "*", // This allows all origins
-//     methods: ["GET", "POST", "PUT", "DELETE"], // Customize based on your needs
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
-// // A simple test route
-// app.get("/", (req, res) => res.send("Hello from WebSocket server"));
-
-// const server = app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Server is running on port ${process.env.PORT || 3000}`);
-// });
-
-// const wss = new WebSocket.Server({ server });
-
-// wss.on("connection", function connection(ws) {
-//   console.log("A new client connected.");
-//   ws.on("message", function incoming(message) {
-//     console.log("received from client: %s", message);
-//     ws.send(`You sent -> ${message}`);
-//     console.log("Sent data to Unity: ", message);
-//   });
-//   ws.on("close", () => console.log("Client disconnected"));
-//   ws.send("Welcome to the secure WebSocket server!");
-// });
-
-// process.on("SIGTERM", () => {
-//   console.log("Process terminating...");
-//   // Close your database connections, stop background tasks, etc.
-//   server.close(() => {
-//     console.log("Process exited");
-//   });
-// });
-
-// wss://craftcast-ws-365cc828bed7.herokuapp.com/
