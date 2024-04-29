9b9d946309078d99b69c11b77a7d898c276aec04
Vinh Nguyen <VinhNuwin@protonmail.com>
2024-04-28 20:12:56 -0700

    Removed interval

--- websocket-server.js
+++ websocket-server.js
@@ -68,19 +68,3 @@ process.on("SIGTERM", () => {
     console.log("Process exited");
   });
 });
-
-const interval = setInterval(() => {
-  wss.clients.forEach((ws) => {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(null, false, true);
-  });
-}, 30000); // Ping every 30 seconds
-
-wss.on("connection", (ws) => {
-  ws.isAlive = true;
-  ws.on("pong", () => {
-    ws.isAlive = true;
-  });
-});
